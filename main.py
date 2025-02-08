01a23b731dac8e1f07120ac7c848c7c4fa88995f
sanagul3498 <sanagul3498@gmail.com>
2025-02-08 13:42:40 +0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from chatbot import chat
+from fastapi.responses import JSONResponse
+from fastapi.middleware import CORSMiddleware
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Chatbot API",
+    description="This is a Chatbot API",
+    version="0.1",
+    docs_url="/",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/ask")
+def ask(question:str):
+    response = chat(question)
+    return JSONResponse(content={"response":response})
+
